2474eefe021f35cc12388cd950acf4e7e0312587
aeteseb <113799213+aeteseb@users.noreply.github.com>
2024-04-24 19:39:22 +0000

    add assistant settings + api

---
+++ prpnext/assistant/app_settings.py
@@ -0,0 +1,44 @@
+import frappe
+
+@frappe.whitelist()
+def get_assistant_settings():
+    user = frappe.session.user
+    print(user)
+    try:
+        app_settings = frappe.get_doc("Assistant App Settings", user)
+        print(app_settings)
+    except frappe.DoesNotExistError as e:
+        print(e)
+        print("Creating new settings")
+        app_settings = frappe.new_doc("Assistant App Settings")
+        app_settings.check_permission(permtype='write')
+        app_settings.user = user
+        app_settings.theme_mode = "dark"
+        app_settings.seed_color = "FF8BC34A"
+        app_settings.insert(ignore_permissions=True)
+        frappe.db.commit()
+
+        print(app_settings, app_settings.name)
+    
+    app_settings = frappe.get_doc("Assistant App Settings", user)
+    return {
+        "themeMode": app_settings.theme_mode,
+        "seedColor": app_settings.seed_color
+    }
+
+@frappe.whitelist()
+def set_theme_mode(theme_mode):
+    user = frappe.session.user
+    app_settings = frappe.get_doc("Assistant App Settings", user)
+    app_settings.theme_mode = theme_mode
+    app_settings.save(ignore_permissions=True)
+    
+
+@frappe.whitelist()
+def set_seed_color(seed_color):
+    user = frappe.session.user
+    app_settings = frappe.get_doc("Assistant App Settings", user)
+    app_settings.seed_color = seed_color
+    app_settings.save(ignore_permissions=True)
+    
+    return app_settings.seed_color
